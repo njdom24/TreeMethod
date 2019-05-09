e495a0743588003d16205a7e82b8401f67284bd6
Dominick DiMaggio <Dom32400@aol.com>
2019-05-09 19:13:14 -0400

    Overhauled Branching System
    Now properly delves through all branches recursively

--- Tree.rkt
+++ Tree.rkt
@@ -1,124 +1,98 @@
 #lang racket
-
-;TODO: Combine terms and negated concl into one list, and use parser functions to split them
-;Assumes terms is all ors, and concl is all ands (when negated)
-(define (Tree terms concl)
-  ;(traverse (cadr terms) (cadr (_not concl)));;assumes input rn
-  ;(get-ands (append terms (_not concl)) '())
-  (traverse (append (get-branches terms '()) (get-branches (_not concl) '()))
-            (append (get-ands terms '()) (get-ands (_not concl) '()))
-            )
-)
-
-
-
-(define (get-branches expr total)
-  (if (null? expr)
-      total
-      (if (equal? (car expr) "or")
-          (get-branches (cddr expr) (append total (cadr expr)))
-          (get-branches (cddr expr) total)
-      )
-  )
-)
-
-(define (get-ands expr total)
-  (if (null? expr)
-      total
-      (if (equal? (car expr) "and")
-          (get-branches (cddr expr) (append total (cadr expr)))
-          (get-branches (cddr expr) total)
-      )
-  )
-)
-
-(define (traverse branches ands)
-  (if (null? branches)
-      #t
-      (and (traverse (cdr branches) ands) (exec-branch (append branches ands)))
-      )
-)
-
-;(Tree (_implies (_or '("H" "W")) "M") (_or '("M" "H")))
-;(Tree (_implies (_or "H" "W") "M") (_or "M" "H"))
-(define (traverses branches ands)
-  (if (null? branches)
-      #t
-      ;(and (exec-branch (append (car branches) ands)) (traverse (cdr branches) ands));;NEED TO CHECK FOR THE "OR" WHEN PASSING IN BRANCHES TO THE INITIAL CALL
-      (exec-branch (append (cadar branches) ands));testing the first branch, assuming no further branching
-      )
-)
-
-(define (exec-branch expr)
-  (if (null? expr)
-      #t
-      (if (null? (extract-branches(clean-ands expr '()) '()));if no branches
-          (check-conflicts (clean-ands expr '()))
-          (check-conflicts (remove-branches(clean-ands expr '())))
-          )
-     
-      )
-  ;expr
-  ;(check-conflicts (clean-ands expr '()));;;PUT A CHECK FOR ORS IN CHECK-CONFLICTS
-);something about check-conflicts
-
-(define (clean-ands lst return)
-  (if (null? lst)
-      return
+;negate the last one
+(define (tree-helper lst return)
+  (if (null? (cdr lst));one element left
       (if (list? (car lst))
-          (if (equal? (caar lst) "and")
-              (clean-ands (cdr lst) (append (cadar lst) return));add the nested and elements
-              (clean-ands (cdr lst) (cons (car lst) return));add the element
+          (append return (list (_not (car lst))))
+          (append return (list(_not (car lst))))
+          )
+      (tree-helper (cdr lst) (append return (list(car lst))))
+      )
+)
+;(tree-helper '("A" "B" "C" ("or" ("D" "B")) "D") '())
+(define (tree . lst)
+  (exec (tree-helper lst '()) '())
+)
+
+(define (tree1 . lst)
+  (tree-helper lst '())
+)
+
+;(tree (_implies (_or "H" "W") "M") (_or "M" "H"))
+;(tree (_implies (_or "H" "W") "M") "M")
+
+;> (exec '(("or" ("!B" "!D")) "C" "B" "A") '())
+;#f
+;> (exec '("!B" "C" "B" "A") '())
+;#f
+;> (exec '("D" "C" "B" "A") '())
+;#t
+
+(define (exec lst totals)
+  (if (null? lst)
+      (check-conflicts totals);return true if no conflicts
+      (if (list? (car lst))
+          (if (equal? (caar lst) "or")
+              (branch (cadar lst) (cdr lst) totals)
+              (if (equal? (caar lst) "and")
+                  (exec (append (cdr lst)(cadar lst)) totals)
+                  (exec (append lst(cadar lst)) totals)
+                  )
+              
               )
-          ;skip over ors
-          (clean-ands (cdr lst) (cons (car lst) return));add the or back in, evaluate later
-          )
-      )
-)
-;USED AFTER clean-ands
-(define (remove-branches lst return)
-  (if (null? lst)
-      return
-      (if (list? (car lst))
-          (remove-branches (cdr lst) return);remove the or
-          (remove-branches (cdr lst) (cons (car lst) return));add regular element back into list
+              (exec (cdr lst) (append totals (list(car lst))))
           )
       )
 )
 
-(define (extract-branches lst return)
-  (if (null? lst)
-      return
-      (if (list? (car lst))
-          (extract-branches (cdr lst) (append (car lst) return));add or back into list
-          (extract-branches (cdr lst) return);remove the regular element
+
+;(list (_or "P" (_or "Q" "!R")) (_implies "P" "!R") (_implies "Q" "!R") "R")
+;(tree '("or" ("P" ("or" ("Q" "!R")))) '("or" ("!P" "!R")) '("or" ("!Q" "!R")) "!R")
+(define (branch2 sublst lst totals)
+  (if (null? sublst)
+      #t
+      (and (branch (cdr sublst) lst totals)
+           (if (list? (car sublst))
+               (exec (append (car sublst) lst) totals)
+               (exec lst (append totals (list(car sublst))))
+               ))
+      )
+)
+
+(define (branch sublst lst totals)
+  (if (null? sublst)
+      #t
+      (if (list? (car sublst))
+          (and (exec (append lst (list (car sublst))) totals)
+               (branch (cdr sublst) lst totals))
+          (and (exec lst (append totals (list(car sublst))))
+               (branch (cdr sublst) lst totals))
           )
       )
 )
 
-;(clean-ands '(("and" ("!H" "!W")) "M" "!M" "!H") '())
+;(tree (_implies (_or "H" "W") "M") (_or "M" "H"))
+;'("or" (("and" ("!H" "!W")) "M"))
+;'("and" ("!M" "!H"))
 
 ;if the string has a ! in front, check if its next character exists in the list
 ;if no !, then check for a !, then check for a matching char
 ;returns true if no conflicts
+(define (check-conflicts2 lst)
+  lst
+)
 (define (check-conflicts lst)
+  (not (conflict-helper lst))
+)
+
+(define (conflict-helper lst)
   (if (null? lst)
       #t
       (and (if (equal? #\! (car(string->list (car lst))));if string starts with negation
                (check-helper (list->string(list(cadr(string->list (car lst))))) (cdr lst));check if the letter is in the rest of the list
                (check-helper (string-append "!" (car lst)) (cdr lst));check if negated version is in the list
             )    
-           (check-conflicts (cdr lst)))
-      )
-)
-
-(define (clean-ors lst return)
-  (if (null? lst)
-      return
-      (if (list? (car lst))
-          (clean-ands (cdr lst) (append (cadar lst) return))
-          (clean-ands (cdr lst) (cons (car lst) return))
-          )
+           (conflict-helper (cdr lst)))
       )
 )
 
